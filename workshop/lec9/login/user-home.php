90f959de460cd404a151c9f669b982f75fd4bcd9
PataninThongsongsom <s6404062660011@email.kmutnb.ac.th>
2023-10-04 15:12:25 +0700

    edit lab9 ver 1

--- workshop/lec9/login/user-home.php
+++ workshop/lec9/login/user-home.php
@@ -3,6 +3,19 @@
 <html>
 <body>
 <h1>สวัสดี <?=$_SESSION["fullname"]?></h1>
+<h2>สิทธิ์ <?=$_SESSION["pevi"]?></h2>
+<?php
+		$stmt = $pdo->prepare("SELECT * FROM product JOIN item ON product.pid = item.pid JOIN orders ON orders.ord_id = item.ord_id JOIN member ON member.username = orders.username WHERE member.username= ? AND member.password=?");
+		$stmt->execute();
+		while ($row = $stmt->fetch()) { 
+	?>
+		<div style="padding: 15px; text-align: center">
+			<h3>
+                <?=$row["pname"] ?><br>
+                <?=$row["quantity"] ?>
+            </h3>
+		</div>
+	<?php } ?>
 หากต้องการออกจากระบบโปรดคลิก <a href='logout.php'>ออกจากระบบ</a>
 </body>
 </html>

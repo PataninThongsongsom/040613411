beb3017d522b4a29941bccb305e3b122e88896d3
PataninThongsongsom <s6404062660011@email.kmutnb.ac.th>
2023-10-05 10:24:44 +0700

    edit userhome

--- lab10/login/user-home.php
+++ lab10/login/user-home.php
@@ -12,7 +12,7 @@
 
     <?php
     include "./connect.php";
-		$stmt = $pdo->prepare("SELECT * FROM product JOIN item ON product.pid = item.pid JOIN orders ON orders.ord_id = item.ord_id JOIN member ON member.username = orders.username WHERE member.username=? GROUP BY product.pname" );
+		$stmt = $pdo->prepare("SELECT product.pname,SUM(item.quantity)AS quantity FROM product JOIN item ON product.pid = item.pid JOIN orders ON orders.ord_id = item.ord_id JOIN member ON member.username = orders.username WHERE member.username=? GROUP BY product.pname" );
 		$stmt->bindParam(1, $_SESSION["username"]);
         $stmt->execute();
         if($_SESSION["pevi"]==='member'){
@@ -34,7 +34,7 @@
         </tr>
 	<?php }}?>
     <?php 
-        $stmt2 = $pdo->prepare("SELECT * FROM product JOIN item ON product.pid = item.pid JOIN orders ON orders.ord_id = item.ord_id JOIN member ON member.username = orders.username GROUP BY member.username,product.pname" );
+        $stmt2 = $pdo->prepare("SELECT orders.ord_id,member.username,product.pname,SUM(item.quantity)AS quantity FROM product JOIN item ON product.pid = item.pid JOIN orders ON orders.ord_id = item.ord_id JOIN member ON member.username = orders.username  GROUP BY product.pname,orders.ord_id;" );
         $stmt2->execute();
         if ($_SESSION["pevi"]==='admin') {
         while ($row2 = $stmt2->fetch()) { 
@@ -43,16 +43,19 @@
                 let th4 =document.createElement("th");
                 let th5 =document.createElement("th");
                 let th6 =document.createElement("th");
+                let th7 =document.createElement("th");
                 let tr2 =document.getElementById("tr1");
+                th7.innerHTML = "OrderID";
                 th4.innerHTML = "Username";
                 th5.innerHTML = "Product";
                 th6.innerHTML = "Quantity";
+                tr2.appendChild(th7);
                 tr2.appendChild(th4);
                 tr2.appendChild(th5);
                 tr2.appendChild(th6);  
             </script>
                 <tr>
-                    
+                    <td><?=$row2["ord_id"]?></td>
                     <td><?=$row2["username"]?></td>
                     <td><?=$row2["pname"]?></td>
                     <td><?=$row2["quantity"]?></td>
